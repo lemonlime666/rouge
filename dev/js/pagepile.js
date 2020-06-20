b1ed5c71829c69d2302b20802af0891d34cb9f1f
lemonlime666 <bnbwade881@gmail.com>
2020-06-20 22:36:52 +0800

    modify_Sat

--- dev/js/pagepile.js
+++ dev/js/pagepile.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     $('#pagepiling').pagepiling({
         menu: null,
         direction: 'horizontal',
-        scrollingSpeed: 500,
+        scrollingSpeed: 800,
         loopBottom: true,
         navigation: {
             'textColor': '#efefef',
