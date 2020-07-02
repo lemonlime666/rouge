ddfdaca5b84b0e8f381bd4b8d5c87667c6e3c10c
s4664588 <s4664588@gmail.com>
2020-07-02 21:17:54 +0800

    update成功

--- dev/php/light-lips.php
+++ dev/php/light-lips.php
@@ -1,6 +1,11 @@
 <?php
 try{
-  require_once("connect.php");
+	$dsn = "mysql:host=localhost;port=3306;dbname=rouge;charset=utf8";
+	$user = "root";
+	$password = "123456";
+	$options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION);
+	$pdo = new PDO($dsn, $user, $password, $options);
+	// echo "連線成功~<br>";
   $sql = "select * from series";
   $series = $pdo->query($sql);
 
