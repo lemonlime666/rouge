3f65314fccf738640a23b7ae80384d55e2dfe09c
louis61619 <louis61619@gmail.com>
2020-07-06 18:47:33 +0800

    add 改為connect

--- dev/php/light-lips.php
+++ dev/php/light-lips.php
@@ -1,12 +1,8 @@
 <?php
 try{
-	$dsn = "mysql:host=localhost;port=3306;dbname=rouge;charset=utf8";
-	$user = "root";
-	$password = "123456";
-	$options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION);
-	$pdo = new PDO($dsn, $user, $password, $options);
+	require_once("connect.php");
 	// echo "連線成功~<br>";
-  $sql = "select * from series";
+  $sql = "select * from series where PRO_CLASS = 0";
   $series = $pdo->query($sql);
 
 
