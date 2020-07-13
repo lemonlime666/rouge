fd103862bc5f5c09ffa9422c0274e85e68252f2d
Dingli0511 <dingli511@gmail.com>
2020-07-13 21:24:26 +0800

    新增試妝頁面,JS,scss,php

--- dev/js/makeUp.js
+++ dev/js/makeUp.js
@@ -1,10 +1,251 @@
 
-$("#M_btn1").click(function(){
-    $("#M_cpModel").fadeIn();
-    $("#M_cpLipColor").hide();
-})
 
-$("#M_btn2").click(function(){
-    $("#M_cpLipColor").fadeIn("1000");
-    $("#M_cpModel").hide();
-})
+$(document).ready(function () {
+    // let my_pro_class = '';
+    //接ajax
+    $.ajax({
+        url: './php/makeUp.php',
+        // data:{
+        //     pro_class:0,
+        //     ser_name:''
+        // },
+        type: 'GET',
+        success(data) {
+            let s = JSON.parse(data);
+            console.log(s)
+            // document.querySelector(".M_title").innerHTML = data;
+            document.querySelector(".M_title").innerHTML = s[0];
+            document.querySelector(".M_productImg").innerHTML = s[1];
+            document.querySelector(".M_detail").innerHTML = s[2];
+        }
+    })
+
+
+
+
+
+    // Get the modal
+    var modal = document.getElementById('id01');
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+    //ROUGE動畫 start
+    var M_text = document.querySelector(".M_bgf");
+    var M_stringText = M_text.textContent;
+    // 切割成字串
+    var M_splitText = M_stringText.split("");
+    //原有p標籤文字消失
+    M_text.textContent = "";
+
+    for (var i = 0; i < M_splitText.length; i++) {
+        //寫入HTML 每個字串前後加標籤 並且指定M_splitText每個i
+        M_text.innerHTML += "<span>" + M_splitText[i] + "</span>";
+    }
+
+    var M_word = 0;
+    var M_time = setInterval(appearSlow, 50);
+
+    function appearSlow() {
+        const span = M_text.querySelectorAll('span')[M_word]; //字從第零開始
+        span.classList.add('M_fade');
+        M_word++
+        if (M_word == M_splitText.length) {
+            M_complete();
+            return;
+        }
+    }
+    function M_complete() {
+        clearInterval(M_time);
+        M_time = null;
+    }
+    //ROUGE動畫 end
+
+
+
+    //最外層tab function 預設 hide&show
+    $(function () {
+        $(".M_option .M_optionGroup").hide();
+        $(".M_option .M_optionGroup:first-child").show();
+
+        // model & color tabclick function
+        $(".M_catalog button").click(function () {
+            $(".M_catalog button").removeClass("M_active");
+            $(this).addClass("M_active");
+
+            var M_makeUpCurrent_tab_value = $(this).attr("data-list");
+            $(".M_option .M_optionGroup").hide();
+            $("." + M_makeUpCurrent_tab_value).slideDown(600);
+        });
+    })
+
+
+
+    // products & color tab function 
+    $(function () {
+        $(".M_menu .M_seriesPanel").hide();
+        $(".M_menu:first-child .M_seriesPanel").show();
+
+        $(".M_menu button").click(function (e) {
+            e.preventDefault();
+
+            //點 ul.lipsticksSer > li.M_menu > btn.M_contorl 顯示.M_seriesPanel 再找.M_seriesPanel收合
+            $(this).siblings(".M_seriesPanel").slideDown().parent().siblings().find(".M_seriesPanel").slideUp();
+
+            //點 ul.lipsticksSer > li.M_menu > btn.M_contorl 加上class M_btn_panel_active
+            $(this).addClass("M_btn_panel_active").parent().siblings().find(".M_control").removeClass("M_btn_panel_active");
+        });
+    });
+
+
+
+
+    //變更model
+    $(function () {
+        $(".M_first button img").click(function () {
+            // 1.先選MODEL
+            changeModel = $(this).attr("id").substr(13);
+            $("#M_bigModel").attr("src", "./image/model/model0" + changeModel + "-hei-res-non.png");
+
+            // 2.再選唇色
+            $(" .M_seriesPanel #color1").click(function () {
+                changeLipColor = $(this).attr("id").substr(5);
+                $("#M_bigModel").attr("src", "./image/model/model0" + changeModel + "-hei-res-" + changeLipColor + ".png")
+            })
+
+            $(" .M_second #lipred").click(function () {
+                changeLipColor = $(this).attr("id").substr(3);
+                $("#M_bigModel").attr("src", "./image/model/model0" + changeModel + "-hei-res-" + changeLipColor + ".png")
+            })
+
+            $(" .M_second #liporange").click(function () {
+                changeLipColor = $(this).attr("id").substr(3);
+                $("#M_bigModel").attr("src", "./image/model/model0" + changeModel + "-hei-res-" + changeLipColor + ".png")
+            })
+
+            $(" .M_second .M_pcnone").click(function () {
+                changeLipColor = $(this).attr("id").substr(3);
+                $("#M_bigModel").attr("src", "./image/model/model0" + changeModel + "-hei-res-" + changeLipColor + ".png")
+            })
+        })
+    })
+
+
+
+    //上傳照片function
+    //1.點上傳照片按鈕切換版面+下載btn
+    // $(function () {
+    //     $(".M_imggrop").hide();
+    //     $("#M_chooseMode6").click(function () {
+
+    //         if ($(this).text() == "上傳照片") {
+    //             $(this).text("取消上傳");
+    //             $("#M_download").show();
+    //             $("#M_Modeldownload").hide();
+    //         }
+    //         else {
+    //             $(this).text("上傳照片");
+    //             $("#M_download").hide();
+    //             $("#M_Modeldownload").show();
+    //         }
+
+    //         $("#M_bigModel").toggle();
+    //         $(".M_imggrop").toggle();
+    //     })
+    // })
+
+    //2.點擊上傳檔案function
+    var M_uploadimg = document.querySelector("#M_uploadimg");
+
+    //點上傳照片 = 點擊連結input file btn
+    $('#M_customBtn').click(function () {
+        M_uploadimg.click();
+    })
+
+    //draw begin
+    const myFile = document.querySelector('#M_uploadimg');
+    const canvas = document.querySelector('#painter');
+    const ctx = canvas.getContext('2d');
+
+    myFile.addEventListener('change', function (e) {
+        const file = e.target.files[0]
+        // 宣告一個新圖片
+        let img = new Image();
+        img.src = URL.createObjectURL(file);
+
+        img.onload = function () {
+            // 設定 canvas 寬高等同圖片
+            // canvas.width = img.width
+            // canvas.height = img.height
+            // img.width = canvas.width
+            // img.height = canvas.height
+            // 設定繪製的圖片寬高
+            ctx.clearRect(0, 0, 700, 700);
+            ctx.drawImage(img, 0, 0, canvas.width, canvas.height)
+        }
+    })
+
+    ctx.strokeStyle = '#9d3333';
+    ctx.lineJoin = 'round';
+    ctx.lineCap = 'round';
+    ctx.lineWidth = 6;
+
+    let isDrawing = false;
+    let lastX = 0;
+    let lastY = 0;
+    let hue = 100;  // 0;
+    let direction = true;
+
+    function draw(e) {
+        if (!isDrawing) return;
+        console.log(e)
+        ctx.strokeStyle = '#9d3333';
+        // ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+        ctx.beginPath();
+        ctx.moveTo(lastX, lastY);
+
+        ctx.lineTo(e.offsetX, e.offsetY);
+        ctx.stroke();
+
+        [lastX, lastY] = [e.offsetX, e.offsetY];
+    }
+
+    canvas.addEventListener('mousedown', (e) => {
+        isDrawing = true;
+
+        [lastX, lastY] = [e.offsetX, e.offsetY];
+    });
+    canvas.addEventListener('mousemove', draw);
+    canvas.addEventListener('mouseup', () => isDrawing = false);
+    canvas.addEventListener('mouseout', () => isDrawing = false);
+    // draw end
+
+
+
+
+    //下載Model圖
+    // var Mimg = document.querySelector('.M_mimg');
+
+    // $("#M_Modeldownload").click(function () {
+    //     var imagePath = Mimg.getAttribute('src');
+    //     var fileModelName = getModelFileName(imagePath);
+
+    //     saveAs(imagePath, fileModelName);
+    // });
+
+    // function getModelFileName(str) {
+    //     return str.substring(str.lastIndexOf('-') + 1);
+    // }
+
+
+
+
+
+
+});
+
+
---
+++ dev/makeUpPage.php
@@ -0,0 +1,276 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Make UP</title>
+    <link rel="stylesheet" href="./css/makeUp.css">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100;300;400;500;700;900&family=Noto+Serif+TC:wght@200;300;400;500;600;700;900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Nunito+Sans:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./css/all.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="./js/filesaver.js"></script>
+</head>
+
+<body>
+    <section class="M_background">
+        <p class="M_bgf">ROUGE</p>
+        <div class="M_line"></div>
+        <!-- 燈箱 -->
+        <div id="id01" class="modal">
+            <form class="modal-content animate" method="post">
+                <div class="container">
+                    <h1>前往明信片製作頁面</h1>
+                    <p>點選下方確定鈕，幫照片裝飾得更美麗！</p>
+                    <div>
+                        <button type="button" onclick="document.getElementById('id01').style.display='none'" class="btn2-1">返回</button>
+                        <!-- <button type="button" onclick="location.href='card.html'" class="btn3-1">確定</button> -->
+                        <button type="button" class="btn3-1" onclick="savingImage()">確定</button>
+
+                    </div>
+                </div>
+            </form>
+        </div>
+        <!-- 燈箱 -->
+        <main class="M_makeup">
+            <div class="M_left">
+                <!-- option button tab -->
+                <div class="M_catalog">
+                    <button class="M_opt" data-list="M_first">MODEL</button>
+                    <button class="M_opt" data-list="M_second">LIP STICKS</button>
+                </div>
+                <div class="M_option" id="M_mainOption">
+                    <!-- model tab start -->
+                    <div class="M_optionGroup M_first">
+                        <button class="M_btn"><img src="./image/model/model01-hei-res-non.png" alt="" style="width: 60%;" id="M_chooseModel1" onclick="loadImage1()"></button>
+                        <button class="M_btn"><img src="./image/model/model02-hei-res-non.png" alt="" style="width: 60%;" id="M_chooseModel2" onclick="loadImage2()"></button>
+                        <button class="M_btn"><img src="./image/model/model03-hei-res-non.png" alt="" style="width: 60%;" id="M_chooseModel3" onclick="loadImage3()"></button>
+                        <button class="M_btn"><img src="./image/model/model04-hei-res-non.png" alt="" style="width: 60%;" id="M_chooseModel4" onclick="loadImage4()"></button>
+                        <button class="M_btn"><img src="./image/model/model05-hei-res-non.png" alt="" style="width: 60%;" id="M_chooseModel5" onclick="loadImage5()"></button>
+                        <button class="M_btnSec" id="M_chooseMode6">上傳照片</button>
+                    </div>
+                    <!-- model tab end -->
+
+                    <!-- products & color tab start -->
+                    <div class="M_optionGroup M_second">
+                        <ul class="M_lipsticksSer" id="M_menu-app">
+                            <?php
+                            try {
+                                require_once("./php/connect.php");
+                                //抓取兩個表格資料 撈出口紅資料共4筆
+                                $sql1 = "SELECT count(distinct SER_NAME) FROM product, series where product.SER_NO=series.SER_NO AND PRO_COLOR LIKE '%'";
+                                $series_name = $pdo->prepare($sql1);
+                                $series_name->execute();
+                                $no_of_ser = $series_name->fetch();
+                                $val_of_color_counter = 0;  //動態新增ID
+                                for ($i = 1; $i <= $no_of_ser[0]; $i++) {
+                                    //最外層li
+                                    echo '<li class="M_menu">
+                                    <button class="M_control">';
+                                    $sql_ser_name = "SELECT distinct SER_NAME FROM product, series where product.SER_NO=series.SER_NO AND PRO_COLOR LIKE '%' and series.SER_NO = $i";
+                                    $MAKEUP_URL_1 = $pdo->prepare($sql_ser_name);
+                                    $MAKEUP_URL_1->execute();
+                                    $ser_name = $MAKEUP_URL_1->fetch();
+                                    echo "$ser_name[0] </button>  <div class=\"M_seriesPanel\">";  //內層顏色div開始
+                                    //撈取資料庫顏色指令
+                                    $sql_ser_ind_color = "SELECT PRO_COLOR FROM product, series where product.SER_NO=series.SER_NO AND PRO_COLOR LIKE '%' and series.ser_no = $i";
+                                    $MAKEUP_colors = $pdo->query($sql_ser_ind_color);  //撈取後端全部資料
+                                    while ($color = $MAKEUP_colors->fetch()) {
+                                        $val_of_color_counter++;  //動態新增ID
+                                        echo "<div class=\"M_pcColor\" style=\"background-color:$color[0];\" id=\"color$val_of_color_counter\"></div>";
+                                    }
+                                    echo '<div class="M_pcnone" ><i class="fas fa-ban fa-4x"></i></div>';
+                                    echo '</li>';
+                                }
+                            } catch (PDOException $e) {
+                                echo $e->getMessage();
+                            }
+                            ?>
+                        </ul>
+                    </div>
+                    <!-- products & color tab end -->
+                </div>
+            </div>
+            <!-- middle content model & customize pic -->
+            <div class="M_middle">
+                <!-- <img class="M_img M_mimg" src="./image/model/model02-hei-res-non.png" alt="" id="M_bigModel"> -->
+                <div class="M_imggrop">
+                    <form class="M_imagecus" id="M_imageGroup" method="POST">
+                        <input type="hidden" name="myImage" id="hidden_data">
+                        <canvas class="M_canvas" id="painter" width="500" height="580"></canvas>
+                        <p class="M_text">尚未選擇檔案</p>
+                    </form>
+                    <input id="M_uploadimg" type="file" hidden>
+                    <button id="M_customBtn">選擇檔案</button>
+                </div>
+            </div>
+            <div class="M_right">
+                <h1 class="M_title">系列名稱</h1>
+                <img src="./image/product.gif" alt="" style="width: 65%;" class="M_productImg" />
+                <div class="M_content">
+                    <!-- <h3 class="M_titleSec">完美持色飽和</h3> -->
+                    <p class="M_detail">描述描述描述</p>
+                </div>
+                <div class="M_group">
+                    <button class="M_contentbtn">加入購物車</button>
+                    <button class="M_contentbtn" onclick="document.getElementById('id01').style.display='block'">製作明信片</button>
+                </div>
+                <div class="M_group">
+                    <button class="M_social"><i class="fab fa-facebook fa-4x"></i></button>
+                    <button class="M_social"><i class="fab fa-line fa-4x"></i></button>
+                    <button class="M_social" id="M_download" onclick="M_saveImage()"><i class="fas fa-file-download fa-4x"></i></button>
+                    <!-- <button class="M_social" id="M_Modeldownload"><i class="fas fa-file-download fa-4x"></i></button> -->
+                </div>
+            </div>
+        </main>
+    </section>
+
+
+    <!-- PHONE -->
+
+    <div class="M_rightgroup">
+        <button class="M_shoppingBag"><i class="fas fa-shopping-bag fa-3x"></i></button>
+        <button class="M_shoppingBag" onclick="location.href='card.html'"><i class="far fa-address-card fa-3x"></i></button>
+    </div>
+
+
+    <script src="./js/makeUp.js"></script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.11/vue.js'></script>
+
+    <script>
+        //下載自製圖
+        function M_saveImage() {
+            const canvasImg = document.querySelector('#painter');
+            canvasImg.toBlob(function(blob) {
+                saveAs(blob, 'test.png')
+            })
+        }
+
+        //model & 自製圖
+
+        let modelSrc = "";
+        // window.addEventListener("load", function() {
+        //         modelSrc = "upload";
+        // })
+
+
+        function loadImage1() {
+            var canvas = document.getElementById("painter");
+            var ctx = canvas.getContext("2d");
+            var img = new Image();
+            img.onload = function() {
+                ctx.clearRect(0, 0, 700, 700);
+                ctx.drawImage(img, 0, 0, 500, 600); //drawImage(img,x,y,width,height)
+            }
+            modelSrc = img.src = document.getElementById("M_chooseModel1").src;
+            alert(modelSrc);
+        }
+
+        function loadImage2() {
+            var canvas = document.getElementById("painter");
+            var ctx = canvas.getContext("2d");
+            var img = new Image();
+            img.onload = function() {
+                ctx.clearRect(0, 0, 700, 700);
+                ctx.drawImage(img, 0, 0, 500, 600); //drawImage(img,x,y,width,height)
+            }
+            modelSrc = img.src = document.getElementById("M_chooseModel2").src;
+            alert(modelSrc);
+        }
+
+        function loadImage3() {
+            var canvas = document.getElementById("painter");
+            var ctx = canvas.getContext("2d");
+            var img = new Image();
+            img.onload = function() {
+                ctx.clearRect(0, 0, 700, 700);
+                ctx.drawImage(img, 0, 0, 500, 600); //drawImage(img,x,y,width,height)
+            }
+            modelSrc = img.src = document.getElementById("M_chooseModel3").src;
+            alert(modelSrc);
+        }
+
+        function loadImage4() {
+            var canvas = document.getElementById("painter");
+            var ctx = canvas.getContext("2d");
+            var img = new Image();
+            img.onload = function() {
+                ctx.clearRect(0, 0, 700, 700);
+                ctx.drawImage(img, 0, 0, 500, 600); //drawImage(img,x,y,width,height)
+            }
+            modelSrc = img.src = document.getElementById("M_chooseModel4").src;
+            alert(modelSrc);
+
+        }
+
+        function loadImage5() {
+            var canvas = document.getElementById("painter");
+            var ctx = canvas.getContext("2d");
+            var img = new Image();
+            img.onload = function() {
+                ctx.clearRect(0, 0, 700, 700);
+                ctx.drawImage(img, 0, 0, 500, 600); //drawImage(img,x,y,width,height)
+            }
+            modelSrc = img.src = document.getElementById("M_chooseModel5").src;
+            alert(modelSrc);
+        }
+
+        //存圖到資料庫
+        function savingImage() {
+            document.getElementById('id01').style.display='block';
+            var M_canvas = document.getElementById("painter");
+            var dataURL = M_canvas.toDataURL("image/png");
+            document.getElementById('hidden_data').value = dataURL;
+            var formData = new FormData(document.getElementById("M_imageGroup"));
+
+            var xhr = new XMLHttpRequest();
+            xhr.onload = function() {
+                if (xhr.status == 200) {
+
+                    if (xhr.responseText == "error") {
+                        alert("Error");
+                    } else {
+                        alert('Succesfully uploaded');
+
+                    }
+                } else {
+                    alert(xhr.status)
+                }
+            }
+
+            xhr.open('POST', './php/makeUp.php', true);
+            xhr.send(formData);
+        }
+    </script>
+
+    <script>
+        window.addEventListener("load", function() {
+            let circles = document.querySelectorAll(".M_pcColor");
+            for (let i = 0; i < circles.length; i++) {
+                circles[i].onclick = function(e) {
+                    alert(modelSrc + e.target.id.substr(5));
+                    modelSrc = img.src = document.getElementById("M_chooseModel1").src;
+                    // modelSrc.attr("src", "./image/model/model0" + changeModel + "-hei-res-" + changeLipColor + ".png")
+
+                    // function changeImg() {
+                    //     var canvas = document.getElementById("painter");
+                    //     var ctx = canvas.getContext("2d");
+                    //     var img = new Image();
+                    //     img.onload = function() {
+                    //         ctx.clearRect(0, 0, 700, 700);
+                    //         ctx.drawImage(img, 0, 0, 500, 600); //drawImage(img,x,y,width,height)
+                    //     }
+                    //     modelSrc = img.src = document.getElementById("color01").src;
+
+                    // }
+                }
+
+            }
+        })
+    </script>
+
+
+</body>
+
+</html>
