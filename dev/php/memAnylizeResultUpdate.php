86db7bb75f5c093c355e85e8942c6da349e065f7
snowfox <kingdog3289@gmail.com>
2020-07-14 17:01:48 +0800

    no message

--- dev/php/memAnylizeResultUpdate.php
+++ dev/php/memAnylizeResultUpdate.php
@@ -1,25 +1,25 @@
 <?php
 
 try{
-  require_once("connect.php");
-  $resultData = json_decode($_POST["signUpInfo"]);
   
-  $sqlresult = "update `maintenance` SET `MTC_CLASS` = 3, `MTC_DATE` = :today WHERE `maintenance`.`MTC_RSL` = 1"
+  require_once("connect.php"); 
+  $sqlresult = "INSERT INTO `maintenance` (`MEM_NO`, `MTC_CLASS`, `MTC_DATE`) VALUES ('1', :class, :today);";
   // $sqlresult = "select * FROM product WHERE MTC_CLASS=:answer";
   $pdoresult = $pdo->prepare($sqlresult);
-  $pdoresult->bindValue(":today", $resultData->today);
+  $pdoresult->bindValue(":today", $_POST['today']);
+  
   // $pdoresult->bindValue(":answer", $_POST["answer"]); //memNO
-  // $pdoresult->bindValue(":class", $_POST["class"]);
+  $pdoresult->bindValue(":class", $_POST["class"]);
   $pdoresult->execute();
 
   //如果找得資料，取回資料，送出xml文件
-  if($pdoresult->rowCount()==0){
-    echo "{}";
-}else{
-    // $result = $pdoresult ->fetchALL(PDO::FETCH_ASSOC);
-    // echo json_encode($result);
+//   if($pdoresult->rowCount()==0){
+//     echo "{}";
+// }else{
+//     // $result = $pdoresult ->fetchALL(PDO::FETCH_ASSOC);
+//     // echo json_encode($result);
     
-}
+// }
 }catch(PDOException $e){
   $err = array("error" => "system error~");
   echo json_encode($err);
