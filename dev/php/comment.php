5a54a6ef62bdabacc56b5e6932d61b6052a7399f
clement <howpines@gmail.com>
2020-07-15 14:52:46 +0800

    留言更新

--- dev/php/comment.php
+++ dev/php/comment.php
@@ -1,17 +1,31 @@
 <?php
 $commentInfo = json_decode($_POST["commentInfo"]);
-try{
-  require_once("connect.php");
-    $sql = "insert into `message` (SER_NO, MEM_NO, MES_DATE, MES_TEXT, MES_STATUS) values (:serno, :memno, :mesdate, :mestext ,'0')";
-    $procomment = $pdo->prepare($sql);
-    $procomment->bindValue(":serno", $commentInfo->SER_NO);
-    $procomment->bindValue(":memno", $commentInfo->MEM_NO);
-    $procomment->bindValue(":mesdate", $commentInfo->MES_DATE);
-    $procomment->bindValue(":mestext", $commentInfo->MES_TEXT);
-    $procomment->execute();
-  
-}catch(PDOException $e){
-  echo $e->getMessage();
+$datetime = date ("Y-m-d");
 
+
+session_start();
+if( isset($_SESSION["mail"])){ //已登入
+    $memInfo = array("memNo"=>$_SESSION["memNo"],"name"=>$_SESSION["name"], "mail"=>$_SESSION["mail"], "adrs"=>$_SESSION["adrs"], "phone"=>$_SESSION["phone"], "voteD"=>$_SESSION["voteD"]);
+
+    try{
+      require_once("connect.php");
+      $sql = "insert into `message` (SER_NO, MEM_NO, MES_DATE, MES_TEXT, MES_STATUS) values (:serno, :memno, :mesdate, :mestext ,'0')";
+      $procomment = $pdo->prepare($sql);
+      $procomment->bindValue(":serno", $commentInfo->SER_NO);
+      $procomment->bindValue(":memno", $_SESSION["memNo"]);
+      $procomment->bindValue(":mesdate", $datetime);
+      $procomment->bindValue(":mestext", $commentInfo->MES_TEXT);
+      $procomment->execute();
+      echo　"留言成功";
+
+      }catch(PDOException $e){
+        echo $e->getMessage();
+      
+      }
+    // echo json_encode($memInfo);
+}else{ //未登入
+	echo "未登入";
 }
+
+
 ?>
