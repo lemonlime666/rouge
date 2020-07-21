8a1e71fce84f2cef41a62269dbd099aa834754a9
s4664588 <s4664588@gmail.com>
2020-07-21 15:25:11 +0800

    some fix

--- dev/php/saveCard.php
+++ dev/php/saveCard.php
@@ -2,27 +2,32 @@
 
 <?php
 
-$upload_dir = "../image/saveCard//";  //檢查資料夾存不存在
-if( !file_exists($upload_dir)){
-  mkdir($upload_dir);
-}
-$imgDataStr = $_POST['hidden_data'];//收到convas.toDataURL()送來的資料
-$imgDatatext = $_POST['cardText'];
-// echo $_POST['hidden_data'];
-$imgDataStr = str_replace('data:image/png;base64,', '', $imgDataStr); //將檔案格式的資訊拿掉
-// $imgDataStr = str_replace(' ', '+', $imgDataStr);
-$data = base64_decode($imgDataStr);
-//準備好要存的filename
-$fileName = date("Ymd-h-i-s");  //或time()
-$file = $upload_dir . $fileName . ".png";
-$success = file_put_contents($file, $data);
-$file= substr($file,1);
-echo $file;
-echo $success ? $file : 'error';
-
 
 session_start();
 if( isset($_SESSION["mail"])){ //已登入
+  //產生圖檔
+
+  $upload_dir = "../image/saveCard//";  //檢查資料夾存不存在
+    if( !file_exists($upload_dir)){
+      mkdir($upload_dir);
+    }
+    $imgDataStr = $_POST['hidden_data'];//收到convas.toDataURL()送來的資料
+    $imgDatatext = $_POST['cardText'];
+    // echo $_POST['hidden_data'];
+    $imgDataStr = str_replace('data:image/png;base64,', '', $imgDataStr); //將檔案格式的資訊拿掉
+    // $imgDataStr = str_replace(' ', '+', $imgDataStr);
+    $data = base64_decode($imgDataStr);
+    //準備好要存的filename
+    $fileName = date("Ymd-h-i-s");  //或time()
+    $file = $upload_dir . $fileName . ".png";
+    $success = file_put_contents($file, $data);
+    $file= substr($file,1);
+    echo $file;
+    echo $success ? $file : 'error';
+
+
+
+
     $memInfo = array("memNo"=>$_SESSION["memNo"],"name"=>$_SESSION["name"], "mail"=>$_SESSION["mail"], "adrs"=>$_SESSION["adrs"], "phone"=>$_SESSION["phone"], "voteD"=>$_SESSION["voteD"]);
     require_once("connect.php");
 
@@ -71,13 +76,13 @@ if( isset($_SESSION["mail"])){ //已登入
       join members 
       on members.MEM_NO = makeup.MEM_NO 
       and makeup.MEM_NO ='{$_SESSION["memNo"]}'";
-      $fbimgurlrow = $pdo ->query($find);
+      $fbimgurlrow = $pdo ->query($fbimgurl);
       $imgRow = $fbimgurlrow->fetch(PDO::FETCH_ASSOC);
 
       echo "成功的修改了 {$upaffectedRows} 筆資料"."|". $imgRow["afterimgurl"];
     }
 
- 
+
 
     
 
@@ -87,6 +92,8 @@ if( isset($_SESSION["mail"])){ //已登入
     // echo "錯誤原因",$e->getMessage(),"<br>";
   	echo "未登入";
 }
+
+
 ?>
 <?php
 // try{
