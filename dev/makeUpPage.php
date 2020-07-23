3877e520c6e935dbe708474813b56a7e39d0caa7
lemonlime666 <bnbwade881@gmail.com>
2020-07-23 18:21:00 +0800

    icon

--- dev/makeUpPage.php
+++ dev/makeUpPage.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="./images/R.ico" />
+    <link rel="shortcut icon" href="./image/R.ico" />
     <title>ROUGE</title>
     <link rel="stylesheet" href="./css/makeUp.css">
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100;300;400;500;700;900&family=Noto+Serif+TC:wght@200;300;400;500;600;700;900&display=swap" rel="stylesheet">
